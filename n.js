8f9dfdd63831bb69db1daaf8fe88357169e0d1cb
yingfeng ni <lovenyf@163.com>
2013-05-20 22:15:34 +0800

    模块JS开始

---
+++ n.js
@@ -0,0 +1,17 @@
+// 模块加载器
+// nyf 2013.5.20
+
+// 实现模块加载化
+
+(function(global, undefined){
+	'use strict';
+	var O_N = global.N, //原始的 N 对象or属性
+		N = global.N = {},
+		doc = global.document;
+
+	N.defined = function(id, deps, wrap){
+		
+	}
+
+		
+})(window);
