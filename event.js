008ae74bba09307c035b135ae751376e9c7c961d
lovenyf <lovenyf@163.com>
2013-09-24 14:29:12 +0800

    利用data模块实现 简单的事件模块 目前还有点绑定上的问题

--- event.js
+++ event.js
@@ -4,7 +4,7 @@ N.define("event",["data"],function(data){
 
     var doc = document;
 
-    var addEvent = (function( target, name, handler ){
+    var addEvent = (function(){
         if( doc.addEventListener ){
             return function( target, name, handler ){
                 return target.addEventListener(name, handler, false)
@@ -20,17 +20,58 @@ N.define("event",["data"],function(data){
         }
     })();
 
-    var removeEvent = function( target, name, handler ){}
+    var removeEvent = (function(){
+        if( doc.removeEventListener ){
+            return function( target, name, handler ){
+                return target.removeEventListener(name, handler, false)
+            } 
+        }else if( doc.detachEvent ){
+            return function( target, name, handler ){
+                return target.detachEvent("on"+name, handler);
+            }
+        }else{
+            return function( target, name ){
+                return target["on"+name] = null;
+            }
+        }
+    })();
    
     function on( target, name, callback ){
-        data.data( obj, name, callback );
+        var events = data.data( target, name) || [];
+
+        events.push( callback );
+        data.data( target, name, events);
+
+        addEvent( target, name, function(){
+            emit(target, name);
+        });
     }
 
     function off( target, name, callback ){
-        data.removeData( target, name, callback );
+        var events = data.data( target, name) || [];
+
+        if( typeof callback === "function" && events.length !== 0){
+            each( events, function( event , i , events){
+                if( callback === event ){
+                    events.splice(i , 1); 
+                    data.data( target, name, events);
+                }
+            });
+        }else{
+            data.removeData( target, name, callback );
+        }
+
+        
     }
 
-    function emit( target, name ){}
+    function emit( target, name ){
+        var events = data.data( target, name);
+
+        each( events, function(event){
+            event.call( target );
+        });
+
+    }
 
 
     return {
