d2291c7b5d4a67c13fe625548860b25b80f70758
lovenyf <lovenyf@163.com>
2013-11-20 15:04:21 +0800

    调整整体架构

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt){
+
+    // 项目配置
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%=pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }               
+        }
+    });
+
+    // 加载提供"uglify"任务的插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // 默认任务
+    grunt.registerTask('default', ['uglify']);
+}
