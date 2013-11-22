6dbade64031779a2436578986a1379424a1ed7e9
lovenyf <lovenyf@163.com>
2013-11-22 17:57:23 +0800

    无大修改

--- src/css.js
+++ src/css.js
@@ -2,15 +2,21 @@
 // hide show
 // addCss removeCss
 // addClass removeClass
+// toggle
 
 N.define( "css", function(){
     var trim = N.trim,
-        filter = N.filter;
+        filter = N.filter,
+        isArray = N.isArray;
 
     function dealClass( classStr ){
         var classesName = [],
             classStr = trim( classStr );
 
         return classStr.split(/\s+/g);
+    }
+
+    function addClass( doms, classes ){
+        if( !isArray(doms) )
     }
 });
