804c2661c0c6a2c8e13577b2882b39745b9ff64d
lovenyf <lovenyf@163.com>
2014-05-06 20:05:26 +0800

    编写

--- src/lib/template.js
+++ src/lib/template.js
@@ -10,10 +10,56 @@ N.define( 'template', function(){
         escapeReg = /[&<>"\']/g,
 
         transformMethod = {
-            escape : /<%=([\s\S]+?)%>/,
-            unescape : /<%-([\s\S]+?)%>/,
-            normal : /<%([\s\S]+?)%>/,
-            noMatch : /(.)^/
+            escape : '<%=([\s\S]+?)%>',
+            unescape : '<%-([\s\S]+?)%>',
+            normal : '<%([\s\S]+?)%>'
         },
-        transformReg = new Regexp();
+        transformReg = new RegExp([
+            transformMethod.escape,
+            transformMethod.unescape,
+            transformMethod.normal].join('|'), 'g');
+
+    var esacpeFunc = function( str ){
+        return str.replace( escapeReg, function( match ){
+            return escapeMap[ match ];
+        } );
+    };
+
+
+    return function( tmpl, data ){
+        var tmplStr = "_s+='",
+            index = 0;
+
+        tmpl.replace( transformReg, function( match, escape, unescape, normal, offset){
+            tmplStr += tmpl.slice(index, offset);
+
+            if( escape ){
+                tmplStr += "';+\n((" + escape + ")==null?'':esacpeFunc(escape))+\n'"
+            }else if( unescape ){
+                tmplStr += "';+\n((" + unescape + ")==null?'':unescape)+\n'"
+            }else if( normal ){
+                tmplStr += "';+\n" + normal + "\n'"
+            }
+
+            index = offset + match.length;
+        } );
+    }
+
+    // <%=name%>aaa<%if(c){%>bbb<%}%>ccc
+    function (data){
+        var _s = '';
+
+        with(data){
+            _s += name;
+            _s += 'aaa';
+            if(c){
+            _s += 'bbb'
+            }
+            _s += 'ccc'
+        }
+
+        return _s;
+
+    }
+
 } );
