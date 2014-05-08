c8812c06d4fa606e52a2eb786cdeea6f6ab5a4dd
lovenyf <lovenyf@163.com>
2014-05-08 22:06:46 +0800

    template 完成 未测试

--- src/lib/template.js
+++ src/lib/template.js
@@ -10,9 +10,9 @@ N.define( 'template', function(){
         escapeReg = /[&<>"\']/g,
 
         transformMethod = {
-            escape : '<%=([\s\S]+?)%>',
-            unescape : '<%-([\s\S]+?)%>',
-            normal : '<%([\s\S]+?)%>'
+            escape : '<%=([\\s\\S]+?)%>',
+            unescape : '<%-([\\s\\S]+?)%>',
+            normal : '<%([\\s\\S]+?)%>'
         },
         transformReg = new RegExp([
             transformMethod.escape,
@@ -28,38 +28,48 @@ N.define( 'template', function(){
 
     return function( tmpl, data ){
         var tmplStr = "_s+='",
-            index = 0;
+            index = 0, len = tmpl.length;
 
+        var c = tmpl.replace( transformReg, 'a');
         tmpl.replace( transformReg, function( match, escape, unescape, normal, offset){
+
             tmplStr += tmpl.slice(index, offset);
 
             if( escape ){
-                tmplStr += "';+\n((" + escape + ")==null?'':esacpeFunc(escape))+\n'"
+                tmplStr += "';\n_s+=((" + escape + ")==null?'':esacpeFunc(" + escape + "));\n_s+='"
             }else if( unescape ){
-                tmplStr += "';+\n((" + unescape + ")==null?'':unescape)+\n'"
+                tmplStr += "';\n_s+=((" + unescape + ")==null?'':" + unescape + ");\n_s+='"
             }else if( normal ){
-                tmplStr += "';+\n" + normal + "\n'"
+                tmplStr += "';\n" + normal + ";\n_s+='"
             }
 
             index = offset + match.length;
         } );
+
+        tmplStr += tmpl.slice(index, len) + "'";
+
+        tmplStr = "var _s='';\n with(data){\n "+ tmplStr +"}\n return _s;";
+
+        var tmplFunc = new Function( "data", tmplStr );
+
+        return data ? tmplFunc( data ) : tmplFunc;
     }
 
     // <%=name%>aaa<%if(c){%>bbb<%}%>ccc
-    function (data){
-        var _s = '';
+    // function (data){
+    //     var _s = '';
 
-        with(data){
-            _s += name;
-            _s += 'aaa';
-            if(c){
-            _s += 'bbb'
-            }
-            _s += 'ccc'
-        }
+    //     with(data){
+    //         _s += name;
+    //         _s += 'aaa';
+    //         if(c){
+    //         _s += 'bbb'
+    //         }
+    //         _s += 'ccc'
+    //     }
 
-        return _s;
+    //     return _s;
 
-    }
+    // }
 
 } );
